f75fab591d0c3341b07d6ab8ee4ba06974de53fa
Kiran Anand <liteninkiran@hotmail.com>
2025-04-22 17:22:38 +0100

    Step 6: Arrange the Data

--- js/layout.js
+++ js/layout.js
@@ -1,11 +1,18 @@
 function layout(data) {
+    const cellWidth = config.width / config.numColumns;
+    const cellHeight = cellWidth;
+    const maxRadius = 0.35 * cellWidth;
     const radiusScale = d3.scaleSqrt()
         .domain([0, 100])
-        .range([0, 20]);
+        .range([0, maxRadius]);
     
-    return data.map((d, i) => ({
-        x: i * 10,
-        y: 100,
-        radius: radiusScale(d.renewable),
-    }));
+    return data.map((d, i) => {
+        const column = i % config.numColumns;
+        const row = Math.floor(i / config.numColumns);
+        return {
+            x: (column + 0.5) * cellWidth,
+            y: (row + 0.5) * cellHeight,
+            radius: radiusScale(d.renewable),
+        }
+    });
 }
